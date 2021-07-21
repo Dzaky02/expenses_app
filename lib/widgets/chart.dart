a0139f467b8850d2d7ac74dfa8d584878fb1ab45
Ahmad Dzaky Abrori <broridzaky2702@gmail.com>
2021-07-21 17:54:46 +0700

    Fixing chart bar amount text.

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -26,7 +26,7 @@ class Chart extends StatelessWidget {
         'day': DateFormat.E('in').format(weekDay),
         'amount': totalSum,
       };
-    });
+    }).reversed.toList();
   }
 
   double get totalSpending {
