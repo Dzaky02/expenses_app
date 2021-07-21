ff74c5d11d51054fb23b5832e0e203f6d3671b7d
Ahmad Dzaky Abrori <broridzaky2702@gmail.com>
2021-07-21 13:40:33 +0700

    Edit transaction list card to ListTile.

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -22,45 +22,35 @@ class TransactionList extends StatelessWidget {
             physics: NeverScrollableScrollPhysics(),
             itemCount: transactions.length,
             itemBuilder: (context, index) => Card(
-              child: Row(
-                children: [
-                  Container(
-                    margin: EdgeInsets.symmetric(
-                      vertical: 10,
-                      horizontal: 15,
-                    ),
-                    padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Theme.of(context).primaryColor,
-                        width: 2,
+              elevation: 5,
+              margin: EdgeInsets.symmetric(
+                vertical: 6,
+                horizontal: 16,
+              ),
+              child: ListTile(
+                leading: CircleAvatar(
+                  radius: 30,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: FittedBox(
+                      child: Text(
+                        '\$ ${transactions[index].amount.toStringAsFixed(2)}',
                       ),
-                    ),
-                    child: Text(
-                      '\$ ${transactions[index].amount.toStringAsFixed(2)}',
-                      style: Theme.of(context).textTheme.headline6?.copyWith(
-                          fontWeight: FontWeight.bold,
-                          color: Theme.of(context).primaryColor),
                     ),
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        transactions[index].title,
-                        style: Theme.of(context).textTheme.bodyText1?.copyWith(
-                              fontWeight: FontWeight.bold,
-                            ),
+                ),
+                title: Text(
+                  transactions[index].title,
+                  style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                        fontWeight: FontWeight.bold,
                       ),
-                      Text(
-                        DateFormat('EEEE, dd MMMM y', 'in').format(
-                          transactions[index].date.toLocal(),
-                        ),
-                        style: Theme.of(context).textTheme.caption,
-                      ),
-                    ],
+                ),
+                subtitle: Text(
+                  DateFormat('EEEE, dd MMMM y', 'in').format(
+                    transactions[index].date.toLocal(),
                   ),
-                ],
+                  style: Theme.of(context).textTheme.caption,
+                ),
               ),
             ),
           );
